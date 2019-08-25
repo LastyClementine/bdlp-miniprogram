d1f9ac660db0c1824b40059e332a53559357a17d
lasty <9231718d>
2019-08-26 01:49:41 +0800

    样式调整

--- src/pages/today_achievement/index.js
+++ src/pages/today_achievement/index.js
@@ -2,6 +2,7 @@ import Taro, {Component} from '@tarojs/taro'
 import {View, Text, Image, Button, Canvas} from '@tarojs/components'
 import {connect} from '@tarojs/redux'
 import './index.scss'
+import wxml2canvas from '../../utils/html2canvas/wxml2canvas'
 
 @connect(({today_achievement, common, loading}) => ({
     ...today_achievement,
@@ -36,21 +37,31 @@ export default class Today_achievement extends Component {
                     const {
                         achievement_data
                     } = this.props
-                    console.log('achievement_data',achievement_data);
-                    Taro.getImageInfo({
-                        src:achievement_data.cover,
-                    })
-                        .then(res=>{
-                            console.log('res',res);
-                            Taro.saveImageToPhotosAlbum({
-                                filePath:res.path
-                            })
+
+                    const wrapperId = '.wrapper'
+                    const drawClassName = '.draw'
+                    const canvasId = 'canvasposter'
+
+                    wxml2canvas(wrapperId, drawClassName, canvasId).then(() => {
+                        Taro.canvasToTempFilePath({
+                            canvasId: 'canvasposter',
+                            success(res) {
+                                Taro.getImageInfo({
+                                    src:res.tempFilePath,
+                                })
                                 .then(res=>{
-                                    Taro.showToast({
-                                        title:"图片保存成功"
+                                    Taro.saveImageToPhotosAlbum({
+                                        filePath:res.path
+                                    })
+                                    .then(res=>{
+                                        Taro.showToast({
+                                            title:"图片保存成功"
+                                        })
                                     })
                                 })
+                            }
                         })
+                    })
                 }
             })
     }
@@ -63,45 +74,44 @@ export default class Today_achievement extends Component {
         return (
             <View className='today_achievement-page'>
                 <View className="wrapper">
-                    <Image className="bg-image" src={achievement_data.cover}/>
-                    <Canvas className='canvas-poster' canvas-id='canvasposter'></Canvas>
-                    <View className="content">
-                        <View className="top">
-                            <Image className="header-img" src={achievement_data.user_avatar}/>
-                            <View className="desc">
-                                <View className="name">{achievement_data.nick_name}</View>
-                                <View className="date">{achievement_data.date}</View>
+                    <Image className="bg-image draw" src={achievement_data.cover}/>
+                    <View className="content draw">
+                        <View className="top draw">
+                            <Image className="header-img draw" src={achievement_data.user_avatar}/>
+                            <View className="desc draw">
+                                <View className="name draw" data-text={achievement_data.nick_name}>{achievement_data.nick_name}</View>
+                                <View className="date draw" data-text={achievement_data.date}>{achievement_data.date}</View>
                             </View>
                         </View>
 
-                        <View className="sum-data">
-                            <View className="item">
-                                <View className="desc">累计打卡</View>
-                                <View className="value">
-                                    <View className="num">{achievement_data.user_all_card_num}</View>
-                                    <View className="unit">天</View>
+                        <View className="sum-data draw">
+                            <View className="item draw">
+                                <View className="desc draw" data-text="累计打卡">累计打卡</View>
+                                <View className="value draw">
+                                    <View className="num draw" data-text={achievement_data.user_all_card_num}>{achievement_data.user_all_card_num}</View>
+                                    <View className="unit draw" data-text="天">天</View>
                                 </View>
                             </View>
-                            <View className="item">
-                                <View className="desc">今日步数</View>
-                                <View className="value">
-                                    <View className="num">{achievement_data.user_now_step_num}</View>
-                                    <View className="unit">步</View>
+                            <View className="item draw">
+                                <View className="desc draw" data-text="今日步数">今日步数</View>
+                                <View className="value draw">
+                                    <View className="num draw" data-text={achievement_data.user_now_step_num}>{achievement_data.user_now_step_num}</View>
+                                    <View className="unit draw" data-text="步">步</View>
                                 </View>
                             </View>
                         </View>
 
-                        <View className="footer">
-                            <Text>"</Text>
+                        <View className="footer draw" data-text={achievement_data.word}>
+                            <Text className="draw">"</Text>
                             {achievement_data.word}
-                            <Text>"</Text>
+                            <Text className="draw">"</Text>
                         </View>
                     </View>
                 </View>
-
                 <View className="btn white" onClick={this.saveImage}>
                     保存图片
                 </View>
+                <Canvas className='canvas-poster' canvas-id='canvasposter'></Canvas>
             </View>
         )
     }
